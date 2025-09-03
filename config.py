88af80da21e1eaac98f78373da2a001e74fc00da
ousama-sadek <ousama981661@gmail.com>
2025-09-03 16:08:38 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# ملف الاعدادات config.py
+
+# التوكن من BotFather (انت عطيتني هذا)
+TELEGRAM_TOKEN = "7566190734:AAHdK0wsdXAHunDSDD5W-ZdBE1qQ4zjHDpQ"
+
+# ID تبعك على تيليجرام
+TELEGRAM_USER_ID = 8197627648
+
+# اذا حابب تضيف API لمنصة تداول (مثلا Binance أو غيرها)
+API_KEY = ""
+API_SECRET = ""
